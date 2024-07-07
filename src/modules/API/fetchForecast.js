ec8f1d45b479e1f0557c6b13c49dd9db26756dfc
Dimitrije108 <126205228+Dimitrije108@users.noreply.github.com>
2024-07-07 14:28:39 +0200

    feat: refactor promise based API fetch functions into async/await

--- src/modules/API/fetchForecast.js
+++ src/modules/API/fetchForecast.js
@@ -1,24 +1,26 @@
 const API_KEY = '745413e844de44248cb173813242106';
 const WEATHER_BASE_URL = 'https://api.weatherapi.com/v1/forecast.json';
 // Retrieves current weather as well as the available forecast (Weather API (3days))
-export default function getWeatherData(location) {
-  return fetch(`${WEATHER_BASE_URL}?key=${API_KEY}&days=7&q=${location}`, {
-    mode: 'cors',
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`Status: ${response.status} ${response.statusText}`);
-      }
-      return response.json();
-    })
-    .then((data) => {
-      if (data.error) {
-        throw new Error(`Status: ${data.error.code} ${data.error.message}`);
-      }
-      return data;
-    })
-    .catch((error) => {
-      console.error(error);
-      alert(error);
-    });
+export default async function getWeatherData(location) {
+  try {
+    // Retrieve API
+    const response = await fetch(
+      `${WEATHER_BASE_URL}?key=${API_KEY}&days=7&q=${location}`,
+      { mode: 'cors' }
+    );
+    // Check if response is ok
+    if (!response.ok) {
+      throw new Error(`Status: ${response.status} ${response.statusText}`);
+    }
+    // Process response into data
+    const data = await response.json();
+    // Check if data has an error
+    if (data.error) {
+      throw new Error(`Status: ${data.error.code} ${data.error.message}`);
+    }
+    return data;
+  } catch (error) {
+    console.error(error);
+    alert(error);
+  }
 }
--- src/modules/API/fetchSearch.js
+++ src/modules/API/fetchSearch.js
@@ -1,19 +1,22 @@
 const API_KEY = '745413e844de44248cb173813242106';
 const WEATHER_BASE_URL = 'https://api.weatherapi.com/v1/search.json';
 // Retrieves suggestion locations based on user input
-export default function getSuggestionData(location) {
-  return fetch(`${WEATHER_BASE_URL}?key=${API_KEY}&q=${location}`, {
-    mode: 'cors',
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error(`Status: ${response.status} ${response.statusText}`);
-      }
-      return response.json();
-    })
-    .then((data) => data)
-    .catch((error) => {
-      console.error(error);
-      alert(error);
-    });
+export default async function getSuggestionData(location) {
+  try {
+    // Retrieve API
+    const response = await fetch(
+      `${WEATHER_BASE_URL}?key=${API_KEY}&q=${location}`,
+      { mode: 'cors' }
+    );
+    // Check if response is ok
+    if (!response.ok) {
+      throw new Error(`Status: ${response.status} ${response.statusText}`);
+    }
+    // Process response into data
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    alert(error);
+  }
 }
